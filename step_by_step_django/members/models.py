631c4ccfe74344fb6a9c8d06de1fd8ed97bd8ee3
BBeast131 <yukato.beast@gmail.com>
2025-03-17 22:12:48 -0700

    insert, update, delete data & update model' when you update model, you use

--- step_by_step_django/members/models.py
+++ step_by_step_django/members/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+  firstname = models.CharField(max_length=200)
+  lastname = models.CharField(max_length=200)
+  phone = models.IntegerField(null = True)
+  joined_date = models.DateField(null=True)
+
+### Below Models are ones For Django documentation.
+
+# class Question(models.Model):
+#   question_text = models.CharField(max_length=200)
+#   pub_date = models.DateTimeField("date published")
+
+# class Choice(models.Model):
+#   question = models.ForeignKey(Question, on_delete = models.CASCADE)
+#   choice_text = models.CharField(max_length=200)
+#   votes = models.IntegerField(max_length=200)
