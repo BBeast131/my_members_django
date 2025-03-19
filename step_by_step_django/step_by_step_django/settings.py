145fdfec7a4836b04cef644c71dc4fe628f4606a
BBeast131 <yukato.beast@gmail.com>
2025-03-18 20:02:38 -0700

    display data' master template for componentation landing page 404 test

--- step_by_step_django/step_by_step_django/settings.py
+++ step_by_step_django/step_by_step_django/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g1hyqwgbg)bv_yxr=5&lnkogcm%9rj3h@9ixl*6pf)*23#*ds6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
